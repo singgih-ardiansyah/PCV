b7d4b3b52373cefdb012b3d85e0dc0a1e69d9ea9
Singgih Ardiansyah <65878681+singgih-ardiansyah@users.noreply.github.com>
2022-04-06 10:06:56 +0700

    Add files via upload

---
+++ Video_Replace_Background/VideoBGReplace.py
@@ -0,0 +1,39 @@
+import cv2
+import cvzone
+from cvzone.SelfiSegmentationModule import SelfiSegmentation
+import os
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+segmentor = SelfiSegmentation()
+fpsReader = cvzone.FPS()
+
+listImg = os.listdir("BackgroundImages")
+imgList = []
+for imgPath in listImg:
+    img = cv2.imread(f'BackgroundImages/{imgPath}')
+    imgList.append(img)
+
+indexImg = 0
+
+while True:
+    success, img = cap.read()
+    # imgOut = segmentor.removeBG(img, (255,0,255), threshold=0.83)
+    imgOut = segmentor.removeBG(img, imgList[indexImg], threshold=0.8)
+
+    imgStack = cvzone.stackImages([img, imgOut], 2,1)
+    _, imgStack = fpsReader.update(imgStack)
+    print(indexImg)
+    cv2.imshow("image", imgStack)
+    key = cv2.waitKey(1)
+    if key == ord('a'):
+        if indexImg>0:
+            indexImg -=1
+    elif key == ord('d'):
+        if indexImg<len(imgList)-1:
+            indexImg +=1
+    elif key == ord('q'):
+        break
+
